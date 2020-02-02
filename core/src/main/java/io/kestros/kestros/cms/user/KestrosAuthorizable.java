8bfdd4454e534c4f9ca94b55dda314c1c01f53b8
Danny Moulton <ddmoulton@gmail.com>
2020-02-01 19:28:39 -0500

    kestros user service initial commit

---
+++ core/src/main/java/io/kestros/kestros/cms/user/KestrosAuthorizable.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.kestros.kestros.cms.user;
+
+import io.kestros.commons.structuredslingmodels.BaseResource;
+import io.kestros.kestros.cms.user.group.KestrosUserGroup;
+import io.kestros.kestros.cms.user.services.KestrosUserService;
+import java.util.List;
+import javax.annotation.Nonnull;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.OSGiService;
+
+@Model(adaptables = Resource.class)
+public class KestrosAuthorizable extends BaseResource {
+
+  @OSGiService
+  private KestrosUserService userService;
+
+  @Nonnull
+  public String getTitle() {
+    return getId();
+  }
+
+  /**
+   * The current User or Group's unique ID.
+   *
+   * @return The current User or Group's unique ID.
+   */
+  @Nonnull
+  public String getId() {
+    return getProperties().get("rep:principalName", StringUtils.EMPTY);
+  }
+
+  public List<KestrosUserGroup> getMemberOf() {
+    return userService.getUserGroupsForAuthorizable(this, getResourceResolver());
+  }
+
+}
---
+++ core/src/main/java/io/kestros/kestros/cms/user/services/KestrosUserService.java
@@ -0,0 +1,63 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.kestros.kestros.cms.user.services;
+
+import io.kestros.kestros.cms.user.KestrosAuthorizable;
+import io.kestros.kestros.cms.user.KestrosUser;
+import io.kestros.kestros.cms.user.exceptions.UserGroupRetrievalException;
+import io.kestros.kestros.cms.user.exceptions.UserRetrievalException;
+import io.kestros.kestros.cms.user.group.KestrosUserGroup;
+import java.util.List;
+import javax.jcr.RepositoryException;
+import org.apache.sling.api.resource.ResourceResolver;
+
+public interface KestrosUserService {
+
+  /**
+   * Returns the current user.
+   *
+   * @param resourceResolver resourceResolver from the current user.
+   * @return Current User.
+   */
+  KestrosUser getCurrentUser(ResourceResolver resourceResolver) throws UserRetrievalException;
+
+  KestrosUser getUser(String userId, ResourceResolver resourceResolver)
+      throws UserRetrievalException;
+
+  List<KestrosUser> getAllKestrosUsers(ResourceResolver resourceResolver);
+
+  KestrosUserGroup getKestrosUserGroup(String groupId, ResourceResolver resourceResolver)
+      throws UserGroupRetrievalException;
+
+  List<KestrosUserGroup> getAllKestrosUserGroups(ResourceResolver resourceResolver);
+
+  List<KestrosUserGroup> getUserGroupsForAuthorizable(KestrosAuthorizable authorizable,
+      ResourceResolver resourceResolver);
+
+  List<KestrosAuthorizable> getGroupMemberAuthorizables(KestrosUserGroup group,
+      ResourceResolver resourceResolver) throws UserGroupRetrievalException, RepositoryException;
+
+  List<KestrosUser> getGroupMemberUsers(KestrosUserGroup group, ResourceResolver resourceResolver)
+      throws UserGroupRetrievalException, RepositoryException;
+
+  List<KestrosUserGroup> getGroupMemberGroups(KestrosUserGroup group,
+      ResourceResolver resourceResolver) throws UserGroupRetrievalException, RepositoryException;
+
+}
